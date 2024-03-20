d3cc854f029195a12bae3b2b28e9bd707a9c71b2
takeru1038 <sevasutyan@icloud.com>
2024-03-20 10:27:30 +0700

    Delete unnecessary codes

--- js/accordion.js
+++ js/accordion.js
@@ -1,16 +1,3 @@
-// //アコーディオンをクリックした時の動作
-// $('.title').on('click', function() {//タイトル要素をクリックしたら
-// 	var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
-// 	$(findElm).slideToggle();//アコーディオンの上下動作
-    
-// 	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
-// 		$(this).removeClass('close');//クラス名を除去し
-// 	}else{//それ以外は
-// 		$(this).addClass('close');//クラス名closeを付与
-// 	}
-// });
-
-// nameは実際のテキストに応じて編集
 const header_navi = [
 	{
 		name: "タイを知る",
@@ -43,39 +30,21 @@ const header_navi = [
 ]
 
 $('.header-button').on('click', function() {
-	$(".accordion-area").slideUp(500);
-
 	const title = $(this).children(".title").text();
 	const title_excluded_spaces = title.trim();
-
-	// const flag_open = $(".accordion-area").hasClass("open")
-	// if(flag_open){
-	// 	$(".accordion-area").removeClass("open");
-	// }
 
 	for(let i = 0; i < header_navi.length; i++){
 		if(header_navi[i].name == title_excluded_spaces){
 			const accordion_name = ".accordion-" + header_navi[i].class_name;
 			if($(accordion_name).hasClass("open")){
 					$(accordion_name).removeClass("open");
+					$(".accordion-area").slideUp("fast");
 			} else {
+				$(".accordion-area").css("display", "none");
 				$(".accordion-area").removeClass("open");
 				$(accordion_name).addClass("open");
-				$(accordion_name).slideDown(500);
+				$(accordion_name).slideDown("fast");
 			}
 		}
 	}
 })
-
-
-$('.box').slideUp(500);//クラス名.boxがついたすべてのアコーディオンを閉じる
-    
-	var findElm = $(this).next(".box");//タイトル直後のアコーディオンを行うエリアを取得
-    
-	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
-		$(this).removeClass('close');//クラス名を除去    
-	}else{//それ以外は
-		$('.close').removeClass('close'); //クラス名closeを全て除去した後
-		$(this).addClass('close');//クリックしたタイトルにクラス名closeを付与し
-		$(findElm).slideDown(500);//アコーディオンを開く
-	}
